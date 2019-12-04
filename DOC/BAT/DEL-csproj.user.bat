e2aae543b1a920313514b7c2b521999ac7ce05dd
Siegmar Gieseler <siegmar.gieseler@siegmar.com.br>
2019-12-04 20:19:41 -0300

    -

--- DOC/BAT/DEL-csproj.user.bat
+++
@@ -1,4 +0,0 @@
-@ECHO DEL "*.csproj.user" files
-@ECHO Press any key to START or [CTRL][C] to STOP
-@PAUSE
-FOR /F "tokens=*" %%G IN ('DIR /B /S *.csproj.user') DO DEL /Q "%%G"
--- DOC/BAT/RMDIR-packages.bat
+++
@@ -1,4 +0,0 @@
-@ECHO RMDIR "packages" directories
-@ECHO Press any key to START or [CTRL][C] to STOP
-@PAUSE
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S packages') DO RMDIR /S /Q "%%G"
